b7c47da2425f2bc3e5111e872b8fd6bbe6008abc
VESTADOM\MitinaUV <yulmit@mail.ru>
2018-11-14 13:14:12 +0700

    Lesson10: Add lambda in method doRead in DataStreamSerializer

--- src/com/urise/webapp/storage/serializer/DataStreamSerializer.java
+++ src/com/urise/webapp/storage/serializer/DataStreamSerializer.java
@@ -7,6 +7,7 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.IntStream;
 
 public class DataStreamSerializer implements StreamSerializer {
     private static final String NULL_HOLDER = "Null_Holder";
@@ -92,45 +93,65 @@ public class DataStreamSerializer implements StreamSerializer {
             String fullName = dis.readUTF();
             Resume resume = new Resume(uuid, fullName);
             int sizeContacts = dis.readInt();
-            for (int i = 0; i < sizeContacts; i++) {
-                resume.addContact(ContactType.valueOf(dis.readUTF()), dis.readUTF());
-            }
+            IntStream.range(0, sizeContacts).forEach(val -> {
+                try {
+                    resume.addContact(ContactType.valueOf(dis.readUTF()), dis.readUTF());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            });
 
             int sizeSections = dis.readInt();
-            for (int i = 0; i < sizeSections; i++) {
-                final SectionType sectionType = SectionType.valueOf(dis.readUTF());
-                switch (sectionType) {
-                    case OBJECTIVE:
-                    case PERSONAL:
-                        resume.addSection(sectionType, new TextSection(dis.readUTF()));
-                        break;
-                    case ACHIEVEMENT:
-                    case QUALIFICATIONS:
-                        int sizeList = dis.readInt();
-                        final List<String> list = new ArrayList<>();
-                        for (int j = 0; j < sizeList; j++) {
-                            list.add(dis.readUTF());
-                        }
-                        resume.addSection(sectionType, new ListSection(list));
-                        break;
-                    case EXPERIENCE:
-                    case EDUCATION:
-                        int sizeOrganization = dis.readInt();
-                        final List<Organization> listOrganization = new ArrayList<>();
-                        for (int j = 0; j < sizeOrganization; j++) {
-                            String name = dis.readUTF();
-                            String url = readNotRequiredFields(dis);
-                            final List<Organization.Position> listPosition = new ArrayList<>();
-                            int sizePosition = dis.readInt();
-                            for (int k = 0; k < sizePosition; k++) {
-                                listPosition.add(new Organization.Position(LocalDate.parse(dis.readUTF()), LocalDate.parse(dis.readUTF()), dis.readUTF(), readNotRequiredFields(dis)));
-                            }
-                            listOrganization.add(new Organization(new Link(name, url), listPosition));
-                        }
-                        resume.addSection(sectionType, new OrganizationSection(listOrganization));
-                        break;
+            IntStream.range(0, sizeSections).forEach(val -> {
+                try {
+                    final SectionType sectionType = SectionType.valueOf(dis.readUTF());
+                    switch (sectionType) {
+                        case OBJECTIVE:
+                        case PERSONAL:
+                            resume.addSection(sectionType, new TextSection(dis.readUTF()));
+                            break;
+                        case ACHIEVEMENT:
+                        case QUALIFICATIONS:
+                            int sizeList = dis.readInt();
+                            final List<String> list = new ArrayList<>();
+                            IntStream.range(0, sizeList).forEach(val1 -> {
+                                try {
+                                    list.add(dis.readUTF());
+                                } catch (IOException e) {
+                                    e.printStackTrace();
+                                }
+                            });
+                            resume.addSection(sectionType, new ListSection(list));
+                            break;
+                        case EXPERIENCE:
+                        case EDUCATION:
+                            int sizeOrganization = dis.readInt();
+                            final List<Organization> listOrganization = new ArrayList<>();
+                            IntStream.range(0, sizeOrganization).forEach(val1 -> {
+                                try {
+                                    String name = dis.readUTF();
+                                    String url = readNotRequiredFields(dis);
+                                    final List<Organization.Position> listPosition = new ArrayList<>();
+                                    int sizePosition = dis.readInt();
+                                    IntStream.range(0, sizePosition).forEach(val2 -> {
+                                        try {
+                                            listPosition.add(new Organization.Position(LocalDate.parse(dis.readUTF()), LocalDate.parse(dis.readUTF()), dis.readUTF(), readNotRequiredFields(dis)));
+                                        } catch (IOException e) {
+                                            e.printStackTrace();
+                                        }
+                                    });
+                                    listOrganization.add(new Organization(new Link(name, url), listPosition));
+                                } catch (IOException e) {
+                                    e.printStackTrace();
+                                }
+                            });
+                            resume.addSection(sectionType, new OrganizationSection(listOrganization));
+                            break;
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
                 }
-            }
+            });
             return resume;
         }
     }
