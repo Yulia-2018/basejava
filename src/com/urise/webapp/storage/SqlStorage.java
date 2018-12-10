b951e83fb52ab9c725d54399ea6de6d00a58bea4
VESTADOM\MitinaUV <yulmit@mail.ru>
2018-12-10 11:01:04 +0700

    Lesson14: Editing method getAllSorted in SqlStorage

--- src/com/urise/webapp/storage/SqlStorage.java
+++ src/com/urise/webapp/storage/SqlStorage.java
@@ -92,7 +92,7 @@ public class SqlStorage implements Storage {
     public List<Resume> getAllSorted() {
         lOG.info("GetAllSorted");
         List<Resume> resumes = new ArrayList<>();
-        sqlHelper.sqlTransactionalExecute(conn -> {
+        /*sqlHelper.sqlTransactionalExecute(conn -> {
             try (PreparedStatement ps1 = conn.prepareStatement("SELECT * FROM resume r ORDER BY full_name, uuid");
                  PreparedStatement ps2 = conn.prepareStatement("SELECT * FROM contact c WHERE c.resume_uuid = ?")) {
                 ResultSet rs1 = ps1.executeQuery();
@@ -102,6 +102,23 @@ public class SqlStorage implements Storage {
                     ResultSet rs2 = ps2.executeQuery();
                     while (rs2.next()) {
                         addContact(resume, rs2);
+                    }
+                    resumes.add(resume);
+                }
+            }
+            return null;
+        });*/
+        sqlHelper.sqlTransactionalExecute(conn -> {
+            try (PreparedStatement ps1 = conn.prepareStatement("SELECT * FROM resume r ORDER BY full_name, uuid");
+                 PreparedStatement ps2 = conn.prepareStatement("SELECT * FROM contact c")) {
+                ResultSet rs1 = ps1.executeQuery();
+                ResultSet rs2 = ps2.executeQuery();
+                while (rs1.next()) {
+                    Resume resume = new Resume(rs1.getString("uuid"), rs1.getString("full_name"));
+                    while (rs2.next()) {
+                        if (rs1.getString("uuid").equals(rs2.getString("resume_uuid"))) {
+                            addContact(resume, rs2);
+                        }
                     }
                     resumes.add(resume);
                 }
